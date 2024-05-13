b44195785391d13cdda1f2a377419bd9c4c8c654
emir.bolin <emir.bolin@elev.ga.ntig.se>
2024-05-13 11:15:30 +0200

    Server was created.

--- src/MulticastChatServer.java
+++ src/MulticastChatServer.java
@@ -2,36 +2,33 @@ import java.net.*;
 
 public class MulticastChatServer {
 
-    public static void main(String[] args) {
-        throws Exception{
-
-            // Default port number we are going to use
-            int portnumber = 8001;
-            if (args.length >= 1){
-                portnumber = Integer.parseInt(args[0]);
-            }
-
-            // Create a MulticastSocket
-            MulticastSocket serverMulticastSocket = new MulticastSocket(portnumber);
-            System.out.println("MulticastSocket is created at port "+portnumber);
-
-            // Determine the IP address of a host, given the host name
-            InetAddress group = InetAddress.getByName("225.4.5.6");
-
-            // getByName- returns IP address of given host
-            serverMulticastSocket.joinGroup(group);
-            System.out.println("joinGroup method is called...");
-            boolean infinite = true;
-
-            // Continually receives data and prints them
-            while(infinite){
-                byte buf[] = new byte[1024];
-                DatagramPacket data = new DatagramPacket(buf, buf.length);
-                serverMulticastSocket.receive(data);
-                String msg = new String(data.getData().trim());
-                System.out.println("Message received form client = " + msg);
-            }
-            serverMulticastSocket.close();
+    public static void main(String[] args) throws Exception {
+        // Default port number we are going to use
+        int portnumber = 8001;
+        if (args.length >= 1) {
+            portnumber = Integer.parseInt(args[0]);
         }
+
+        // Create a MulticastSocket
+        MulticastSocket serverMulticastSocket = new MulticastSocket(portnumber);
+        System.out.println("MulticastSocket is created at port " + portnumber);
+
+        // Determine the IP address of a host, given the host name
+        InetAddress group = InetAddress.getByName("225.4.5.6");
+
+        // getByName- returns IP address of given host
+        serverMulticastSocket.joinGroup(group);
+        System.out.println("joinGroup method is called...");
+        boolean infinite = true;
+
+        // Continually receives data and prints them
+        while (infinite) {
+            byte buf[] = new byte[1024];
+            DatagramPacket data = new DatagramPacket(buf, buf.length);
+            serverMulticastSocket.receive(data);
+            String msg = new String(data.getData()).trim();
+            System.out.println("Message received form client = " + msg);
+        }
+        serverMulticastSocket.close();
     }
 }
